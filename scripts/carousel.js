7b962834ce40fb22989821a34a1356ff63a5b9c1
Clemestine <celestinepaille@gmail.com>
2025-03-26 14:10:00 +0100

    💫 animate click in carousel

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,7 +1,7 @@
 /** NEXT STEPS
+ * - handle that avoid multiple click : wait the animation to finish
  * - handle when up mouse, go in a standard position with a smooth animation
  * - after this ↑, handle the power of the movement : if the mouse is fast, go in further standard position (and even do complete circle)
- * - handle that the click has a smooth animation
  * - add a slow infinite automatic rotation, stop it when the mouse hover a photo
  */
 
@@ -60,13 +60,14 @@ function getZIndex(pos) {
  * Apply right style to the image depending of its position.
  * Modify z-index, filter, transform and offset-distance.
  * @param {Element} img the image that we apply style
- * @param {number} pos the position of the image, in 0 and 1
+ * @param {number} nextPos the next position of the image, in 0 and 1
  */
-function applyStyleWithoutAnimation(img, pos) {
-  img.style.zIndex = getZIndex(pos);
-  img.style.filter = `brightness(${getBrightness(pos)}%)`;
-  img.style.transform = `scale(${getScale(pos)})`;
-  img.style.offsetDistance = `${pos * 100}%`;
+function applyStyleWithoutAnimation(img, nextPos) {
+  img.style.zIndex = getZIndex(nextPos);
+  img.style.filter = `brightness(${getBrightness(nextPos)}%)`;
+  img.style.transform = `scale(${getScale(nextPos)})`;
+  img.style.offsetDistance = `${nextPos * 100}%`;
+  img.dataset.position = nextPos;
 }
 /**
  * Create and display all the image in the initial position
@@ -89,7 +90,7 @@ function displayImgs() {
     //initial style
     let pos = standardPosImgs[i - 1];
     applyStyleWithoutAnimation(img, pos);
-    img.dataset.position = `${pos}`;
+    img.dataset.position = pos;
     img.draggable = false;
 
     div.appendChild(img);
@@ -141,22 +142,128 @@ function getAngleOfMouse(e) {
   return Math.atan2(startY, startX);
 }
 /**
- * Apply all the right style to move the carousel up to the next position
- * @param {number} deltaPercentage the delta to add to the current position
+ * Get the  index of the closest value in the standard position
+ * @param {number} pos the position of the image, in 0 and 1
+ * @returns {number} the index of the closest value in standard position
  */
-function moveCarousel(deltaPercentage) {
-  //we calculate next position of the carousel ie next position of the first image
-  let nextPosUnconstrained = parseFloat(carousel.dataset.prevPosition) + deltaPercentage;
-  let nextPosition = getConstrainedPos(nextPosUnconstrained);
-  carousel.dataset.position = nextPosition;
+function getClosestIndexStandardPos(pos) {
+  let closestIndex = standardPosImgs.reduce(
+    (closestIdx, current, idx) =>
+      Math.abs(current - pos) < Math.abs(standardPosImgs[closestIdx] - pos) ? idx : closestIdx,
+    0
+  );
+  return closestIndex == standardPosImgs.length - 1 ? 0 : closestIndex;
+}
+/**
+ * Remove each animation style of the image to ensure that element style aren't ignored
+ * @param {Element} img the image
+ */
+function removeAllAnimations(img) {
+  img.getAnimations().forEach((anim) => {
+    anim.commitStyles();
+    anim.cancel();
+  });
+}
+/**
+ * Animate to the right style to the image depending of its position.
+ * Modify z-index, filter, transform and offset-distance.
+ * @param {Element} img the image that we apply style
+ * @param {number} nextPos the next position of the image, in 0 and 1
+ * @param {boolean} isClockwiseRotation the rotation of the animation
+ */
+function applyStyleWithAnimation(img, nextPos, isClockwiseRotation) {
+  let duration = 1000;
+  if (
+    (nextPos < img.dataset.position && !isClockwiseRotation) ||
+    (nextPos > img.dataset.position && isClockwiseRotation)
+  ) {
+    let sizeStep1;
+    let sizeStep2;
+    let step1To;
+    if (isClockwiseRotation) {
+      sizeStep1 = parseFloat(img.dataset.position);
+      sizeStep2 = 1 - nextPos;
+      step1To = 0;
+    } else {
+      sizeStep1 = 1 - img.dataset.position;
+      sizeStep2 = nextPos;
+      step1To = 1;
+    }
+    //we need to force img to cross 0%
+    let sizeAllSteps = sizeStep1 + sizeStep2;
+    let step1Duration = (sizeStep1 / sizeAllSteps) * duration;
+    let step2Duration = (sizeStep2 / sizeAllSteps) * duration;
 
-  //we change style of each image depending of this next position
-  for (let img of listImgCarousel) {
-    let linearPosUnconstrained = nextPosition + (1 / NB_IMG) * getRankImg(img);
-    let linearPos = getConstrainedPos(linearPosUnconstrained);
-    let nextPos = getRealPos(linearPos);
-    applyStyleWithoutAnimation(img, nextPos);
-    img.dataset.position = nextPos;
+    // 1. Animate to 0% or 100%
+    img.animate(
+      {
+        zIndex: [`${getZIndex(img.dataset.position)}`, `${getZIndex(step1To)}`],
+        filter: [`brightness(${getBrightness(img.dataset.position)}%)`, `brightness(${getBrightness(step1To)}%)`],
+        transform: [`scale(${getScale(img.dataset.position)})`, `scale(${getScale(step1To)})`],
+        offsetDistance: [`${img.dataset.position * 100}%`, `${step1To * 100}%`],
+      },
+      { duration: step1Duration, fill: "forwards", easing: "linear" }
+    ).onfinish = () => {
+      // 2. Cross 0% or 100% without visual change
+      let afterCrossing = isClockwiseRotation ? 1 : 0;
+      // 3. Animate from 0% or 100% to real position
+      img.animate(
+        {
+          zIndex: [`${getZIndex(afterCrossing)}`, `${getZIndex(nextPos)}`],
+          filter: [`brightness(${getBrightness(afterCrossing)}%)`, `brightness(${getBrightness(nextPos)}%)`],
+          transform: [`scale(${getScale(afterCrossing)})`, `scale(${getScale(nextPos)})`],
+          offsetDistance: [`${afterCrossing * 100}%`, `${nextPos * 100}%`],
+        },
+        { duration: step2Duration, fill: "forwards", easing: "linear" }
+      ).onfinish = () => {
+        removeAllAnimations(img);
+      };
+    };
+  } else {
+    //usual animation
+    img.animate(
+      {
+        zIndex: [`${getZIndex(img.dataset.position)}`, `${getZIndex(nextPos)}`],
+        filter: [`brightness(${getBrightness(img.dataset.position)}%)`, `brightness(${getBrightness(nextPos)}%)`],
+        transform: [`scale(${getScale(img.dataset.position)})`, `scale(${getScale(nextPos)})`],
+        offsetDistance: [`${img.dataset.position * 100}%`, `${nextPos * 100}%`],
+      },
+      { duration: duration, fill: "forwards", easing: "linear" }
+    ).onfinish = () => {
+      removeAllAnimations(img);
+    };
+  }
+  img.dataset.position = nextPos;
+}
+
+/**
+ * Apply all the right style to move the carousel up to the next position
+ * @param {number} delta the delta to add to the current position, is percentage without animation and index from standardPosImgs with
+ * @param {boolean} needAnimation the boolean that say if we animate or not
+ */
+function moveCarousel(delta, needAnimation) {
+  if (!needAnimation) {
+    //we calculate next position of the carousel ie next position of the first image
+    let nextPosUnconstrainedCarousel = parseFloat(carousel.dataset.prevPosition) + delta;
+    let nextPosCarousel = getConstrainedPos(nextPosUnconstrainedCarousel);
+    carousel.dataset.position = nextPosCarousel;
+
+    //we change style of each image depending of their next position
+    for (let img of listImgCarousel) {
+      let nextLinearPosUnconstrained = nextPosCarousel + (1 / NB_IMG) * getRankImg(img);
+      let nextLinearPos = getConstrainedPos(nextLinearPosUnconstrained);
+      let nextPos = getRealPos(nextLinearPos);
+      applyStyleWithoutAnimation(img, nextPos);
+    }
+  } else {
+    //we animate style of each image depending of their next position
+    for (let img of listImgCarousel) {
+      const indexOfNextPos = (NB_IMG + getClosestIndexStandardPos(img.dataset.position) + delta) % NB_IMG;
+      const nextPos = standardPosImgs[indexOfNextPos];
+      applyStyleWithAnimation(img, nextPos, delta < 0);
+      //we apply the next position of the carousel ie next position of the first image
+      if (getRankImg(img) == 0) carousel.dataset.position = nextPos;
+    }
   }
 }
 
@@ -181,7 +288,7 @@ imgProjects.onmousemove = (e) => {
   //we calculate the delta to add to the current position of the carousel
   let angleDiff = carousel.dataset.mouseDownAt - getAngleOfMouse(e);
   let deltaPercentage = angleDiff / (2 * Math.PI);
-  moveCarousel(deltaPercentage)
+  moveCarousel(deltaPercentage, false);
 };
 
 for (let img of listImgCarousel) {
@@ -193,8 +300,11 @@ for (let img of listImgCarousel) {
     //if there's no click or it's longer than 300ms, do nothing
     if (downDate == undefined || new Date() - downDate > 300) return;
 
-    let deltaPercentage = img.dataset.position < 0.5 ? img.dataset.position * -1 : 1 - img.dataset.position;
-    moveCarousel(deltaPercentage)
+    let deltaIndexStandardPos =
+      img.dataset.position < 0.5
+        ? getClosestIndexStandardPos(img.dataset.position) * -1
+        : NB_IMG - getClosestIndexStandardPos(img.dataset.position);
+    moveCarousel(deltaIndexStandardPos, true);
   });
 }
 
