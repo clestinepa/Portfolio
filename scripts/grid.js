8caf36992bcf37d2174dd023430187c830bdb06a
Clemestine <celestinepaille@gmail.com>
2025-04-20 16:36:55 +0200

    💄grid is now responsive

--- scripts/grid.js
+++ scripts/grid.js
@@ -1,6 +1,25 @@
 /** Constants **/
 const GRID = {
-  NB_IMG: 9, //nb image in the grid
+  ITEM: {
+    WIDTH: {
+      MAX: 300, //(in px) maximum width of an item
+      MIN: 225, //(in px) minimum width of an item
+    },
+    FONT: {
+      H2: {
+        MAX: 22, //(in px) maximum font-size of h2
+        MIN: 18, //(in px) minimum font-size of h2
+      },
+      P: {
+        MAX: 16, //(in px) maximum font-size of p
+        MIN: 12, //(in px) minimum font-size of p
+      },
+      LOGO: {
+        MAX: 350, //(in px) maximum font-size of logo
+        MIN: 225, //(in px) minimum font-size of logo
+      },
+    },
+  },
 };
 /** ********* **/
 
@@ -47,6 +66,34 @@ export async function displayGrid() {
     hover.appendChild(text);
     hover.appendChild(logos);
     div.appendChild(hover);
-    gridContainer.appendChild(div);
+
+    let wrapper = document.createElement("div");
+    wrapper.className = "grid-wrapper";
+    wrapper.appendChild(div);
+    gridContainer.appendChild(wrapper);
   }
+
+  gridContainer.style.setProperty("--max-width-column", GRID.ITEM.WIDTH.MAX + "px");
+  gridContainer.style.setProperty("--min-width-column", GRID.ITEM.WIDTH.MIN + "px");
+  resizeFont();
 }
+
+/**
+ * Calculate the font size depending of the container width with an linear interpolation
+ * @param {number} min minimum font size
+ * @param {number} max maximum font size
+ * @param {number} width width of the container
+ * @returns the associated font size
+ */
+function calcFontSize(min, max, width) {
+  let fontSize = min + (max - min) * ((width - GRID.ITEM.WIDTH.MIN) / (GRID.ITEM.WIDTH.MAX - GRID.ITEM.WIDTH.MIN));
+  return Math.max(min, Math.min(fontSize, max));
+}
+function resizeFont() {
+  const width = document.getElementsByClassName("img-grid").item(0).offsetWidth;
+  const fontSizeH2 = calcFontSize(GRID.ITEM.FONT.H2.MIN, GRID.ITEM.FONT.H2.MAX, width);
+  const fontSizeP = calcFontSize(GRID.ITEM.FONT.P.MIN, GRID.ITEM.FONT.P.MAX, width);
+  gridContainer.style.setProperty("--font-size-h2", fontSizeH2 + "px");
+  gridContainer.style.setProperty("--font-size-p", fontSizeP + "px");
+}
+window.addEventListener("resize", resizeFont);
